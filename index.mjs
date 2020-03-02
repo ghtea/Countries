8e5700f819fffc5d7342d18f6aad88488ef173f2
PJH <eiirwp@gmail.com>
2020-03-02 20:00:12 +0900

    q

--- index.mjs
+++ index.mjs
@@ -1,12 +1,35 @@
 import { html, Component, render } from 'https://ghtea.github.io/Countries/common/standalone.module.js';
 
+/*루트 컴포넌트가 함수형이면 안되는듯?*/
 
-const Root = ({name}) => 
- html`<h1>${name}</h1>`;
+class App extends Component {
+      makeName() {
+        let randomName = arrayFront[Math.floor(Math.random() * arrayFront.length)]['word'] + arrayBack[Math.floor(Math.random() * arrayBack.length)]['word'];
+        
+        const { nicknames = [] } = this.state;
+        this.setState({ nicknames: nicknames.concat(`${randomName}`) });
+        console.log(nicknames);
+      }
+      render({ version }, { nicknames = [] }) {
+        return html`
+          <div class="app">
+            <${Header} name="combiname v${version}" />
+            <button onClick=${() => this.makeName()} >Make Random Nickname</button>
+            
+            <ul>
+              ${nicknames.map(nickname => html`
+                <li>${nickname}</li>
+              `)}
+            </ul>
+           </div>
+        `;
+        
+      }
+    }
 
-render(html`<${Root} name= "hahaha" version="0.1" />`, document.body);
+    const Header = ({ name }) => html`<h1>${name}</h1>`
 
-
+    render(html`<${App} version="0.1" />`, document.body);
 
 
 /*
