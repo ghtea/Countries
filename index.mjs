41832a5c7e1d5350efd7bcea46d4b8abe380d54d
PJH <eiirwp@gmail.com>
2020-03-02 19:42:21 +0900

    q

--- index.mjs
+++ index.mjs
@@ -1,9 +1,13 @@
 import { html, Component, render } from 'https://ghtea.github.io/Countries/common/standalone.module.js';
 
 
-const Root = ({ name }) => html`<h1>${name}</h1>`
+const Root = ({name}) => 
+ html`<h1>${name}</h1>`;
 
 render(html`<${Root} name= "hahaha" version="0.1" />`, document.body);
+
+
+
 
 /*
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="400" height="400" role="img">
