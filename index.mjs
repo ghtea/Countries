9277c34f866f64af1eb23a6981daaffc884132f3
PJH <eiirwp@gmail.com>
2020-03-03 17:27:48 +0900

    q

---
+++ index.mjs
@@ -0,0 +1,24 @@
+/* https://github.com/developit/htm */
+
+import { html, Component, render } from 'https://ghtea.github.io/Countries/common/standalone.module.js';
+
+  /*루트 컴포넌트가 함수형이면 안되는듯?*/
+
+
+  const Test = ({ name }) => html`<h1>${name}</h1>`;
+
+
+  const TestSvg = () => html`
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="400" height="400">
+
+    <g class="bar">
+      <rect width="${200}" height="19"></rect>
+      <text x="45" y="9.5" dy=".35em">4 ${objCountryBasic['4']['Country']}</text>
+    </g>
+  </svg>
+`
+    
+    
+  render(html`<${TestSvg}  name="TestTest" />`, document.body);
+  
+  render(html`<${Test}  name="TestTest" />`, document.body);
