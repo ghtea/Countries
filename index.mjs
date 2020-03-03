1ec9f8ef387678cebcc4c26b737a6c586b751701
PJH <eiirwp@gmail.com>
2020-03-03 20:56:29 +0900

    q

--- index.mjs
+++ index.mjs
@@ -2,23 +2,29 @@
 
 import { html, Component, render } from 'https://ghtea.github.io/Countries/common/standalone.module.js';
 
-  /*루트 컴포넌트가 함수형이면 안되는듯?*/
+
 
 
   const Test = ({ name }) => html`<h1>${name}</h1>`;
 
 
   const TestSvg = () => html`
-  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="400" height="400">
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="400" height="200">
 
     <g class="bar">
-      <rect width="${200}" height="19"></rect>
+      <rect width="${objCountryPop['4']['2019']*1000/1000000*5}" height="19"></rect>
       <text x="45" y="9.5" dy=".35em">4 ${objCountryBasic['4']['Country']}</text>
+      <text x="45" y="${9.5+19}" dy=".35em"> ${objCountryBasic['4']['Alpha2']}</text>
     </g>
   </svg>
-`
+`;
+
+const TestFlag = () => html`  <img class="imgFlag" src="flagsPng/kr.png"/>`;
+
     
     
-  render(html`<${TestSvg}  name="TestTest" />`, document.body);
+   render(html`<${TestFlag}   />`, document.body);
+  
+   render(html`<${TestSvg}   />`, document.body);
   
   render(html`<${Test}  name="TestTest" />`, document.body);
